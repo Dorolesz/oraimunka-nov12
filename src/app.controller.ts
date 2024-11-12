d266d7594290001720f13adb80689d7afd27732f
Petrik Diak <diak@petrik.hu>
2024-11-12 16:30:10 +0100

    suti

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Sutemeny } from './sutemeny';
+import { UpdateSutemenyDto } from './udate-ssutemeny.dto';
+import { CreateSutemenyDto } from './create-sutemeny.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -11,5 +14,82 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  sutik: Sutemeny[] = [
+    {
+      id: 1,
+      nev: 'Tiramisu',
+      laktozMentes: true,
+      db: 5,
+    },
+    {
+      id: 2,
+      nev: 'Dobostorta',
+      laktozMentes: true,
+      db: 1,
+    },
+    {
+      id: 4,
+      nev: 'Krémes',
+      laktozMentes: false,
+      db: 1,
+    },
+  ]
+  nextID = 5;
+  @Get('sutik')
+  sutemenyekListazasa() {
+    return this.sutik
+  }
+
+  @Get('sutik/:sutiid')
+  sutemenyIdAlapja(@Param('sutiid') id: string) {
+    const idSzam = parseInt(id);
+    const suti = this.sutik.find(suti => suti.id == idSzam);
+    if (!suti) { 
+      throw new NotFoundException("Nincs ilyen ID-jű süti")
+    }
+    return suti;
+  }
+
+  @Delete('sutik/::sutiid')
+  sutiTorles(@Param('::sutiid') id: string) {
+    const idSzam = parseInt(id);
+    const idx = this.sutik.findIndex(suti => suti.id == idSzam)
+    this.sutik.splice(idx)
+  }
+
+  @Get('sutiKereses')
+  sutemenyKereses(@Query('kereses') kereses?: string) {
+    if (!kereses) {
+      return this.sutik;
+    }
+    return this.sutik.filter(suti => suti.nev.toLocaleLowerCase().includes(kereses.toLocaleLowerCase()));
+  }
+
+  @Post('ujSuti')
+  ujSuti(@Body() ujSutiAdatok: CreateSutemenyDto) {
+    const ujSutemeny: Sutemeny = {
+      ...ujSutiAdatok,
+      id: this.nextID,
+    }
+    this.nextID++;
+    this.sutik.push(ujSutemeny);
+    return ujSutemeny;
+  }
+
+  @Patch('sutiModositas/:sutiid')
+  sutiModositas(@Param('sutiid') id: string, @Body() sutiAdatok: UpdateSutemenyDto) {
+    const idSzam = parseInt(id);
+    const eredetiSutiID = this.sutik.findIndex(suti => suti.id == idSzam);
+    const eredetiSuti = this.sutik[eredetiSutiID];
+
+
+    const ujSuti: Sutemeny = {
+      ...eredetiSuti,
+      ...sutiAdatok,
+    };
+    this.sutik[eredetiSutiID] = ujSuti;
+    return ujSuti;
   }
 }
